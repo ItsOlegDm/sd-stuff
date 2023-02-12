efa36439e9a61b1e0da31eb40766f3d71e7c457c
ItsOlegDm <itsolegdm@gmail.com>
2023-02-12 14:59:08 +0200

    build 1

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('D:\\my-website\\node_modules\\infima\\dist\\css\\default\\default.css'),
-  require('D:\\my-website\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages'),
-  require('D:\\my-website\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress'),
-  require('D:\\my-website\\src\\css\\custom.css'),
-];
